67ee9d8adba2c25d94b2ee5c4f3709cd02ec95ac
Arhen Santiago <lincoldarhen9900@gmail.com>
2019-06-06 14:13:32 +0800

    Updates

---
+++ precache-manifest.2e6b9cfd227015fa0cde32df79894dda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9928c87fb759a4c8039d1394fc8a4f45",
+    "url": "//rhen99/React-LyricsFinder/index.html"
+  },
+  {
+    "revision": "04f2a0e61789cea70646",
+    "url": "//rhen99/React-LyricsFinder/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "a8f888c1c3ccc698c163",
+    "url": "//rhen99/React-LyricsFinder/static/js/2.ecee10b6.chunk.js"
+  },
+  {
+    "revision": "04f2a0e61789cea70646",
+    "url": "//rhen99/React-LyricsFinder/static/js/main.598528e8.chunk.js"
+  },
+  {
+    "revision": "25d0fe5fbf8c52fbb7f9",
+    "url": "//rhen99/React-LyricsFinder/static/js/runtime~main.a2b0e81b.js"
+  }
+]);
